c303a302f16891caf7b577179a9070acb20f1473
zziboo <ektha772@gmail.com>
2017-06-15 20:21:22 +0900

    http server connect

---
+++ SomiMusic/app/src/main/java/com/example/zziboo/somimusic/HttpService.java
@@ -0,0 +1,99 @@
+package com.example.zziboo.somimusic;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by zziboo on 2017-06-15.
+ */
+
+public class HttpService extends Thread {
+
+    String urlcommand;
+    Handler handler;
+    int commandflag; // 0=list, 1=file
+    Message msg;
+    Object msgobj;
+    Context context;
+
+    public HttpService(String command, Handler handler, Context context){
+        urlcommand = command;
+        this.handler = handler;
+        msgobj = null;
+        this.context = context;
+    }
+
+    @Override
+    public void run() {
+
+        // HTTP 서비스 호출 (서버 IP : 210.125.31.119:8085)
+        String urlstr = "http://210.125.31.119:8080/ads/android"+ "?" + urlcommand;
+
+        try {
+            URL url = new URL(urlstr);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            StringBuilder responseStringBuilder = new StringBuilder();
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                if(urlcommand.equalsIgnoreCase("list")){
+                    BufferedReader bufferedReader;
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    for (;;){
+                        String stringLine = bufferedReader.readLine();
+                        if (stringLine == null ) break;
+                        responseStringBuilder.append(stringLine + "\n");
+                    }
+                    msgobj = responseStringBuilder.toString();
+                    commandflag = 0;
+                    bufferedReader.close();
+                }else{
+                    byte[] bytearr = new byte[4096];
+                    String musicPath = context.getFilesDir().getPath() + "/";
+                    String titlename = urlcommand.split("=")[1];
+                    File file = new File(musicPath + titlename);
+                    OutputStream opstream = new FileOutputStream(file);
+                    InputStream ipstream = connection.getInputStream();
+                    int buffsize;
+                    while((buffsize=ipstream.read(bytearr)) != -1){
+                        opstream.write(bytearr, 0, buffsize);
+                    }
+                    msgobj = file;
+                    commandflag = 1;
+                }
+            }
+
+            connection.disconnect();
+
+            Log.d("buffer", responseStringBuilder.toString());
+
+            msg = handler.obtainMessage();
+            msg.what = commandflag;
+            msg.obj = msgobj;
+            handler.sendMessage(msg);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
--- SomiMusic/app/src/main/java/com/example/zziboo/somimusic/MusicActivity.java
+++ SomiMusic/app/src/main/java/com/example/zziboo/somimusic/MusicActivity.java
@@ -2,16 +2,22 @@ package com.example.zziboo.somimusic;
 
 import android.app.Activity;
 import android.content.Entity;
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,28 +31,51 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MusicActivity extends AppCompatActivity {
     ListView musicList;
     Button playButton, downloadButton;
-    CustomList adapter;
+    ArrayAdapter adapter;
+    String titleName;
 
-    private final Handler httphandle = new Handler(){
-        public void handleMessage(Message msg){
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            String rtnMsg;
             switch(msg.what){
-                case 0:
-                    rtnMsg = (String) msg.obj;
-                    Toast.makeText(getApplicationContext(), rtnMsg + " Service Fail", Toast.LENGTH_SHORT).show();
+                case 0: //list
+                    String[] titleList;
+                    titleList = ((String)msg.obj).split("%");
+                    for(int i=0; i<titleList.length; ++i) {
+                            titleList[i] = titleList[i];
+                    }
+                    adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, titleList);
+                    musicList.setAdapter(adapter);
+                    musicList.setBackgroundColor(Color.CYAN);
+
+                    handler.removeMessages(msg.what);
                     break;
-                case 1:
-                    rtnMsg = (String) msg.obj;
-                    Toast.makeText(getApplicationContext(), rtnMsg + " Service Start", Toast.LENGTH_SHORT).show();
+                case 1: //file
+                    Toast.makeText(getApplicationContext(), ((File)msg.obj).getPath() + " " + ((File)msg.obj).getName(), Toast.LENGTH_SHORT).show();
+
+                    handler.removeMessages(msg.what);
                     break;
             }
+
         }
     };
 
@@ -60,95 +89,28 @@ public class MusicActivity extends AppCompatActivity {
         playButton = (Button) findViewById(R.id.playbtn);
         downloadButton = (Button) findViewById(R.id.downloadbtn);
 
-        adapter = new CustomList(MusicActivity.this);
-
-        // HTTP 서비스 호출 (서버 IP : 210.125.31.119:8085)
-        String url = "210.125.31.119:8085/ads/android";
-        int param = 1;
-        String message = "what????"; // 대규모 문자열 메시지
-        new CallHttp(url, param, message, httphandle).start();
-    }
-
-    //HTTP 호출 스레드
-    private class CallHttp extends Thread{
-        private String url, msg;
-        private int para;
-        private Handler hand;
-
-        public CallHttp(String url, int para, String msg, Handler hand){
-            this.url = url;
-            this.para = para;
-            this.msg = msg;
-            this.hand = hand;
-        }
-
-        public void run(){
-            Message message = hand.obtainMessage();
-            try{
-                HttpClient httpCilent = new DefaultHttpClient();
-                HttpParams httpParams = httpCilent.getParams();
-                HttpPost httpPost = new HttpPost("http://" + url + "?param=" + para);
-                List<NameValuePair> paramList = new ArrayList<NameValuePair>();
-                paramList.add(new BasicNameValuePair("params", msg));
-                httpPost.setEntity(new UrlEncodedFormEntity(paramList, "utf-8"));
-                httpPost.addHeader("text", "plain");
-
-                HttpResponse response = httpCilent.execute(httpPost);
-                String str = EntityUtils.toString(response.getEntity(), "utf-8");
-
-                message.what = 1;
-                message.arg1 = para;
-                message.obj = str;
-                hand.sendMessage(message);
-            }catch(Exception e){
-                message.what = 0;
-                message.arg1 = para;
-                hand.sendMessage(message);
+        new HttpService("list", handler, getApplicationContext()).start(); //HttpService(String command, Handler handler, Context context)
+        musicList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                titleName = musicList.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), titleName, Toast.LENGTH_SHORT).show();
             }
+        });
+    }
+
+    public void OnClick(View view) throws IOException {
+        switch (view.getId()){
+            case R.id.playbtn:
+                String musicPath = getApplicationContext().getFilesDir().getPath() + "/";
+                MediaPlayer mp = new MediaPlayer();
+                mp.setDataSource(musicPath + titleName);
+                mp.isPlaying();
+                break;
+            case R.id.downloadbtn:
+                new HttpService("title="+titleName, handler, getApplicationContext()).start();
+                break;
         }
     }
 
-
-    // Custom List
-    public class CustomList extends BaseAdapter {
-        private final Activity context;
-        public CustomList(Activity context){
-            this.context = context;
-        }
-
-        @Override
-        public int getCount() {
-            return 0;
-            //return msgDatasetVector.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return 0;
-            //return msgDatasetVector.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View view, ViewGroup parent){ //肄쒕갚硫붿냼??
-            LayoutInflater inflater = context.getLayoutInflater();
-            View rowView = inflater.inflate(R.layout.listitem, null, true);
-            ImageView imageItem = (ImageView) rowView.findViewById(R.id.imageItem);
-            TextView titleItem = (TextView) rowView.findViewById(R.id.titleItem);
-            TextView singerItem = (TextView) rowView.findViewById(R.id.singerItem);
-            //imageItem.setImage
-            //titleItem.setText(msgDatasetVector.get(position).getName());
-            //singerItem.setText(msgDatasetVector.get(position).getPhoneNumber());
-            return rowView;
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
 }
